b997289c34ab5ccd14f97ddd9ee7c4848f7134bf
LinckeTheo <tjl@rincon.com>
2024-06-05 14:40:46 -0400

    feat: adds support for formatting between hex, bin and dec

--- c/byte_blade.c
+++ c/byte_blade.c
@@ -8,6 +8,9 @@
 
 static struct option long_options[] = {
   { "help", no_argument, 0, 'h' },
+  { "hex", no_argument, 0, 'x' },
+  { "bin", no_argument, 0, 'b' },
+  { "dec", no_argument, 0, 'd' },
 };
 
 static FILE* fp;
@@ -28,11 +31,17 @@ static void print_help(char* exe)
 {
   printf("\n\nUsage: %s file start end dtype [--help]\n", exe);
   printf("Options:\n");
-  printf("    file  - Input File Name \n");
-  printf("    start - Output File Name \n");
-  printf("    end   - end index\n");
-  printf("    dtype - Format type to read in\n");
-  printf("    --help  - Print this help menu\n");
+  printf("    file      - Input File Name \n");
+  printf("    start     - Output File Name \n");
+  printf("    end       - end index\n");
+  printf("    dtype     - Format type to read in\n");
+  printf("    --help    - Print this help menu\n");
+  printf("\n");
+  printf("    Print Modes: (Higher priority overrides lower priority). Defaults to dec\n");
+  printf("    --hex     - Prints output in hex. Priority: 0\n");
+  printf("    --bin     - Prints output in binary. Priority: 1\n");
+  printf("    --dec     - Prints output in dec mode. Priority: 2\n");
+  printf("\n");
   printf("input_fmt allowed options:\n");
   printf("  CF - Complex float (%zu bytes)\n", sizeof(complex float));
   printf("  SL - Unsigned Integers (%zu bytes)\n", sizeof(uint32_t));
@@ -47,12 +56,35 @@ enum dtype {
   SB = 2,
 };
 
+enum mode {
+  HEX = 0,
+  BIN = 1,
+  DEC = 2,
+};
+
 typedef struct {
-  int dtype;
+  enum dtype dtype;
+  enum mode mode;
   char* filename;
   size_t start;
   size_t end;
+  int hex;
+  int bin;
+  int dec;
 } cli_args;
+
+enum mode get_priority_mode(cli_args* args)
+{
+  if (args->dec) {
+    return DEC;
+  } else if (args->bin) {
+    return BIN;
+  } else if (args->hex) {
+    return HEX;
+  } else {
+    return DEC;
+  }
+}
 
 void print_cf(complex float z)
 {
@@ -73,20 +105,61 @@ void print_cf_array(complex float* arr, size_t len)
   }
 }
 
-void print_sl_array(uint32_t* arr, size_t len)
+void print_sl_bits(uint32_t n)
+{
+  for (int i = 31; i >= 0; i--) {
+    uint32_t mask = 1 << i;
+    printf("%d", (n & mask) ? 1 : 0);
+    if (i % 8 == 0) {
+      printf(" ");
+    }
+  }
+  printf("\n");
+}
+
+void print_sl_array(uint32_t* arr, size_t len, enum mode mode)
 {
   for (int i = 0; i < len; ++i) {
-    printf("%d\n", arr[i]);
+    switch (mode) {
+    case HEX:
+      printf("%X\n", arr[i]);
+      break;
+    case BIN:
+      print_sl_bits(arr[i]);
+      break;
+    case DEC:
+      printf("%d\n", arr[i]);
+      break;
+    }
   }
 }
 
-void print_sb_array(uint8_t* arr, size_t len)
+void print_sb_bits(uint32_t n)
 {
-  for (int i = 0; i < len; ++i) {
-    printf("%d\n", arr[i]);
+  for (int i = 7; i >= 0; i--) {
+    uint32_t mask = 1 << i;
+    printf("%d", (n & mask) ? 1 : 0);
   }
+  printf("\n");
 }
 
+
+void print_sb_array(uint8_t* arr, size_t len, enum mode mode)
+{
+  for (int i = 0; i < len; ++i) {
+    switch (mode) {
+    case HEX:
+      printf("%02X\n", arr[i]);
+      break;
+    case BIN:
+      print_sb_bits(arr[i]);
+      break;
+    case DEC:
+      printf("%d\n", arr[i]);
+      break;
+    }
+  }
+}
 int main(int argc, char** argv)
 {
   atexit(teardown);
@@ -102,14 +175,14 @@ int main(int argc, char** argv)
 
   while ((c = getopt_long(argc, argv, "I:s:e:h", long_options, &ind)) > 0) {
     switch (c) {
-    case 'I':
-
+    case 'x':
+      args.hex = 1;
       break;
-    case 's':
-      args.start = atol(optarg);
+    case 'b':
+      args.bin = 1;
       break;
-    case 'e':
-      args.end = atol(optarg);
+    case 'd':
+      args.dec = 1;
       break;
     case 'h':
       print_help(argv[0]);
@@ -120,23 +193,23 @@ int main(int argc, char** argv)
     }
   }
 
-  if (argc != 5) {
+  if(optind + 4 != argc) {
     print_help(argv[0]);
     return 1;
   }
 
-  args.filename = argv[1];
-  args.start = atol(argv[2]);
-  args.end = atol(argv[3]);
-  
-  if (strcmp(argv[4], "CF") == 0) {
+  args.filename = argv[optind++];
+  args.start = atol(argv[optind++]);
+  args.end = atol(argv[optind++]);
+
+  if (strcmp(argv[optind], "CF") == 0) {
     args.dtype = CF;
-  } else if (strcmp(argv[4], "SL") == 0) {
+  } else if (strcmp(argv[optind], "SL") == 0) {
     args.dtype = SL;
-  } else if (strcmp(argv[4], "SB") == 0) {
+  } else if (strcmp(argv[optind], "SB") == 0) {
     args.dtype = SB;
   } else {
-    printf("Failed to parse data type: %s\n", optarg);
+    printf("Failed to parse data type: %s\n", argv[optind]);
     return 1;
   }
 
@@ -152,6 +225,7 @@ int main(int argc, char** argv)
     // TODO - print reason
     return 1;
   }
+  enum mode mode = get_priority_mode(&args);
 
   if (args.dtype == CF) {
     b_capacity = (args.end - args.start);
@@ -176,7 +250,7 @@ int main(int argc, char** argv)
     }
 
     b_size = fread(buffer, sizeof *typed_buffer, b_capacity, fp);
-    print_sl_array(typed_buffer, b_size);
+    print_sl_array(typed_buffer, b_size, mode);
   } else if (args.dtype == SB) {
     b_capacity = (args.end - args.start);
     uint8_t* typed_buffer = malloc(sizeof *typed_buffer * b_capacity);
@@ -188,7 +262,7 @@ int main(int argc, char** argv)
     }
 
     b_size = fread(buffer, sizeof *typed_buffer, b_capacity, fp);
-    print_sb_array(typed_buffer, b_size);
+    print_sb_array(typed_buffer, b_size, mode);
   } else {
     printf("Unimplemented error\n");
     return 1;
